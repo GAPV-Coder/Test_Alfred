2027beb2c128895aa821ebe1115ef900b59437f3
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2025-04-27 21:23:53 -0500

    Add serializers

---
+++ delivery_services_app/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from delivery_services_app import Address, Driver, Service
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ['id', 'street', 'city', 'latitude', 'longitude']
+        
+class DriverSerializer(serializers.ModelSerializer):
+    location = AddressSerializer()
+    
+    class Meta:
+        fields = ['id', 'name', 'location', 'is_available']
+        
+class ServiceSerializer(serializers.ModelSerializer):
+    pickup_address = AddressSerializer()
+    driver = DriverSerializer()
+    
+    class Meta:
+        model = Service
+        fields = ['id', 'pickup_address', 'driver', 'status', 'estimated_arrival_time', 'created_at']
